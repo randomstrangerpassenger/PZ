9efdba58bf5ca08823168d0eaff8b619d006472f
randomstrangerpassenger <galak2580@gmail.com>
2026-08-20 17:24:25 +0900

    Install canonical Layer 3 generation pointer

---
+++ Iris/media/lua/client/Iris/Data/IrisLayer3DataCurrent.lua
@@ -0,0 +1,19 @@
+-- Generated generation pointer. This is the single runtime visibility switch.
+return {
+    schema_version = "iris_layer3_generation_pointer_v1",
+    generation_id = "dvf33-77777cd0d96552cf24a0446a682b511c2dc16f122489b5bdb674e5b553102f92",
+    index_module = "Iris/Data/IrisLayer3Generations/dvf33-77777cd0d96552cf24a0446a682b511c2dc16f122489b5bdb674e5b553102f92/IrisLayer3DataChunkIndex",
+    chunk_modules = {
+        "Iris/Data/IrisLayer3Generations/dvf33-77777cd0d96552cf24a0446a682b511c2dc16f122489b5bdb674e5b553102f92/Chunks/Chunk001",
+        "Iris/Data/IrisLayer3Generations/dvf33-77777cd0d96552cf24a0446a682b511c2dc16f122489b5bdb674e5b553102f92/Chunks/Chunk002",
+        "Iris/Data/IrisLayer3Generations/dvf33-77777cd0d96552cf24a0446a682b511c2dc16f122489b5bdb674e5b553102f92/Chunks/Chunk003",
+        "Iris/Data/IrisLayer3Generations/dvf33-77777cd0d96552cf24a0446a682b511c2dc16f122489b5bdb674e5b553102f92/Chunks/Chunk004",
+        "Iris/Data/IrisLayer3Generations/dvf33-77777cd0d96552cf24a0446a682b511c2dc16f122489b5bdb674e5b553102f92/Chunks/Chunk005",
+        "Iris/Data/IrisLayer3Generations/dvf33-77777cd0d96552cf24a0446a682b511c2dc16f122489b5bdb674e5b553102f92/Chunks/Chunk006",
+        "Iris/Data/IrisLayer3Generations/dvf33-77777cd0d96552cf24a0446a682b511c2dc16f122489b5bdb674e5b553102f92/Chunks/Chunk007",
+        "Iris/Data/IrisLayer3Generations/dvf33-77777cd0d96552cf24a0446a682b511c2dc16f122489b5bdb674e5b553102f92/Chunks/Chunk008",
+        "Iris/Data/IrisLayer3Generations/dvf33-77777cd0d96552cf24a0446a682b511c2dc16f122489b5bdb674e5b553102f92/Chunks/Chunk009",
+        "Iris/Data/IrisLayer3Generations/dvf33-77777cd0d96552cf24a0446a682b511c2dc16f122489b5bdb674e5b553102f92/Chunks/Chunk010",
+        "Iris/Data/IrisLayer3Generations/dvf33-77777cd0d96552cf24a0446a682b511c2dc16f122489b5bdb674e5b553102f92/Chunks/Chunk011",
+    },
+}
